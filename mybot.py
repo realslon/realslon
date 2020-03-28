736430b6c9407759b3312dd1f6743b7b8b84b78e
realslon <62797002+realslon@users.noreply.github.com>
2020-03-28 15:45:37 +0300

    Update and rename __init__.py to mybot.py

--- mybot.py
+++ mybot.py
@@ -3,7 +3,7 @@ import telebot
 
 
 
-bot = telebot.TeleBot("1086800436:AAHiX4Tw9F4wRT7SaevYrszA1l5zFslYjyY")
+bot = telebot.TeleBot("TOKEN")
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
     bot.reply_to(message, "Грация блять!")
